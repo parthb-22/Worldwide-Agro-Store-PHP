ff792a0d5c2189ed4c3ca364a45285c35529e0ef
parth22 <parthb7200@gmail.com>
2023-12-02 19:50:33 +0530

    admin login eye slash

--- admin/index.php
+++ admin/index.php
@@ -33,8 +33,34 @@ if (isset($_POST['submit'])) {
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
   <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/login.css">
+  <style>
+    .form {
+      position: relative;
+    }
+
+    .login-form input[type="password"] {
+      padding-right: 30px;
+      /* Increase padding-right to accommodate the eye icon */
+    }
+
+    .password-field {
+      position: relative;
+      margin-bottom: 15px;
+      /* Adjust spacing as needed */
+    }
+
+    #togglePassword {
+      position: absolute;
+      top: 35%;
+      right: 5px;
+      /* Adjust as needed */
+      transform: translateY(-50%);
+      cursor: pointer;
+    }
+  </style>
+
 </head>
 
 <body>
@@ -51,15 +77,34 @@ if (isset($_POST['submit'])) {
     <span style="color:green;"><?php echo $success; ?></span>
     <form class="login-form" action="index.php" method="post">
       <input type="text" placeholder="Username" name="username" />
-      <input type="password" placeholder="Password" name="password" />
+      <div class="password-field">
+        <input type="password" placeholder="Password" name="password" id="passwordField" />
+        <i class="fa fa-eye" id="togglePassword"></i>
+      </div>
       <input type="submit" name="submit" value="Login" />
       <a href="../index.php" class="nav-item nav-link">Go to Home Page</a>
-
     </form>
+
 
   </div>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src='js/index.js'></script>
+  <script>
+    const passwordField = document.getElementById("passwordField");
+    const togglePassword = document.getElementById("togglePassword");
+
+    togglePassword.addEventListener("click", function() {
+      const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+      passwordField.setAttribute("type", type);
+      if (type === "password") {
+        togglePassword.classList.remove("fa-eye-slash");
+        togglePassword.classList.add("fa-eye");
+      } else {
+        togglePassword.classList.remove("fa-eye");
+        togglePassword.classList.add("fa-eye-slash");
+      }
+    });
+  </script>
 </body>
 
 </html>
